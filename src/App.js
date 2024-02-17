1fb93afc1969e6a0330698a2293024712177e18c
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-02-17 19:44:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React, { useState, useEffect } from "react";
-import ApiObj from "./FetchData";
+import CharacterComponent from "./components/CharacterComponent.js";
+import CharacterObj from "./FetchData.js";
 
 const App = () => {
-  const [characterData, setCharacterData] = useState([ ]);
+  const [Characters, setCharacters] = useState({});
+  let [pageNo, setPageNo] = useState(1); 
 
-  useEffect(async () => {
-    let fetchData= await ApiObj.fetchData();
-    setCharacterData(fetchData);
-    console.log(fetchData);
-  },[]);
-  
+  useEffect(() => {
+
+    const fetchDataAndSetCharacters = async () => {
+      try {
+        console.log(pageNo);
+        const data = await CharacterObj.GetPeople(pageNo);
+        console.log(data);
+        setCharacters(data);
+       // console.log(data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchDataAndSetCharacters();
+  }, [pageNo]);
+
+ const next= ()=>{
+    let tmp_pageNo=pageNo +1;
+    setPageNo(tmp_pageNo);
+    console.log(pageNo);
+  }
+ const prev= ()=>{
+  let tmp_pageNo=pageNo -1;
+    setPageNo(tmp_pageNo);
+    console.log(pageNo);
+  }
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.  
+  //console.log(Characters);
   return (
-    <div className="App">
+    <div className="App"> 
+      {Characters.previous &&
+        <button onClick={prev}>PREVIOUS</button>
+      }
+      
+      {Characters.next &&
+        <button onClick={next} >NEXT</button>
+      }
+    
       <h1 className="Header">Karakterler</h1>
+      { 
+      Characters.results &&
+      Characters.results.map((characters, index) => (
+        <CharacterComponent key={index} CharacterData={characters} />
+      )) }
     </div>
   );
-}
-
+};
 export default App;
